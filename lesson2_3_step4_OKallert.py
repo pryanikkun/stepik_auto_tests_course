1fb772a2ed4cf861f5510008fa729001f1789eda
Name LastName <name56464@domen.ru>
2022-08-03 13:07:47 +0300

    it's an exc about allert

---
+++ lesson2_3_step4_OKallert.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element(By.TAG_NAME,"button")
+    button.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.ID,"answer")
+    input1.send_keys(y)
+
+    button = browser.find_element(By.TAG_NAME,"button")
+    button.click()
+
+
+finally:
+    time.sleep(10)
+    browser.quit()
